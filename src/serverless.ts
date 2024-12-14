1147d49033f95253360b30dd1d378b8a1734d1b1
Oséias Costa <oseiasc2@gmail.com>
2024-12-14 19:21:09 -0300

    chore: install package class-validator and class-transformer

--- src/serverless.ts
+++ src/serverless.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
+import { ValidationPipe } from '@nestjs/common';
 
 let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.init();
   const expressApp = app.getHttpAdapter().getInstance();
 
